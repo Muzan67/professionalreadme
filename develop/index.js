277721445beb9bb78931e622df7a82e47d51daf0
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-06-12 23:34:46 -0400

    updated indexjs with prompts

--- develop/index.js
+++ develop/index.js
@@ -1,7 +1,122 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username? (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter GitHub username?');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address? (Required)',
+        validate: emailInput => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log('Please enter your email address?');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'project',
+        message: 'What is your projects name? (Required)',
+        validate: projectInput => {
+          if (projectInput) {
+            return true;
+          } else {
+            console.log('Please enter your projects name?');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'checkbox',
+        name: 'licenses',
+        message: 'What kind of liceses should your project have? (Choose your license)',
+        choices: ['GNU', 'Apache License 2.0', 'Berkeley Software Distributions', 'MIT']
+      },
+      {
+        type: 'input',
+        name: 'dependencies',
+        message: 'What command should be run to install dependencies (Required)',
+        validate: dependenciesInput => {
+          if (dependenciesInput) {
+            return true;
+          } else {
+            console.log('You need to enter a installation dependecy!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'command',
+        message: 'what command should be run to run the tests? (Required)',
+        validate: testInput => {
+          if (testInput) {
+            return true;
+          } else {
+            console.log('Please enter your command to run the tests?');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'project',
+        message: 'What does the user need to know about using this repo? (Required)',
+        validate: repoInput => {
+          if (repoInput) {
+            return true;
+          } else {
+            console.log('Please enter user information about using this repo?');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'project',
+        message: 'What does the user need to know about contributing to this repo? (Required)',
+        validate: contributionInput => {
+          if (conrtibutionInput) {
+            return true;
+          } else {
+            console.log('Please enter contribution information about using this repo?');
+            return false;
+          }
+        }
+      },
+    ]);
+  };
+  
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
